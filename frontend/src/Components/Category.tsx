c1a7b037a8c06cb659a52107b9c4e6b98836b1b1
Carson Burke <carsonburke22@gmail.com>
2022-06-24 21:37:14 -0700

    Fix to random color generator

--- frontend/src/Components/Category.tsx
+++ frontend/src/Components/Category.tsx
@@ -5,7 +5,7 @@ export interface CategoryProps {
   tasks: TaskProps[];
   addTask: (task: TaskProps) => void;
 }
-console.log(`rbg(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`)
+
 const Category = (props: CategoryProps) => {
   return (
     <div className="category">
@@ -16,7 +16,7 @@ const Category = (props: CategoryProps) => {
           // Maybe we just have default values, then make the task editable
           name: "New Task",
           description: "Task Description",
-          color: `rbg(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`,
+          color: `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`,
         })}}
         >Add task</button>
       {props.tasks.map((task, index) => {
