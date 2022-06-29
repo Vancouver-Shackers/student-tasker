4dd1fcde5807e69f58eeebf80f432886ab1b9df3
Carson Burke <carsonburke22@gmail.com>
2022-06-28 18:34:44 -0700

    Added link for connect student account

--- frontend/src/Components/Login.tsx
+++ frontend/src/Components/Login.tsx
@@ -14,13 +14,14 @@ export const Login = (props: {
     <div className="loginParent">
       <div className="loginChild mainBG">
 		  <img src="studentTasker2.png" alt="" className="loginIcon" />
-        <button
+{/*         <button
           className="button ascend secondaryBG"
           onClick={() => fetchData()}
         >
           {" "}
           Connect student account{" "}
-        </button>
+        </button> */}
+		<a className="button ascend secondaryBG" target="blank" href="https://go.microsoft.com/fwlink/p/?LinkID=2123761&amp;lm=deeplink&amp;lmsrc=NeutralHomePageWeb&amp;cmpid=FreemiumSignUpHero-Experiment&amp;tfloptin=true&amp;clcid=0x1009&amp;culture=en-ca&amp;country=ca"> Connect student account </a>
         <button
           className="button ascend secondaryBG"
           onClick={() => props.setLoggedIn(true)}
